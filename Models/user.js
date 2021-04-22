13ab94e8d07fbc5da5bdc65ee209cf07f96d603d
Joni Vilokki <36036801+Gogodijo@users.noreply.github.com>
2021-04-22 12:17:06 +0300

    jiafio

--- Models/user.js
+++ Models/user.js
@@ -15,14 +15,23 @@ const UserSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
-    isLoggedIn: {
-        type:Boolean,
-        default: false
-    },
     refresh_token: {
         type:String,
         default: ""
+    },
+    avatar_url: {
+      type:String,
+      default: ""
     }
+}, {timestamps:true})
+
+UserSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString(),
+    delete returnedObject.isVerified,
+    delete returnedObject.password,
+    delete returnedObject.isAdmin
+  }
 })
 
 const User = mongoose.model('User', UserSchema)
--- index.js
+++ index.js
@@ -1,37 +1,9 @@
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const bodyparser = require('body-parser')
-app.use(bodyparser.json())
-const cors = require('cors')
+const http = require('http')
+const app = require('./app')
 
-app.listen(3001, (err)=> {
-    if(err){
-        console.log(err)
-    }
-    else{
-        console.log("Server running ")
-    }
+const server = http.createServer(app)
+
+server.listen("3001", (err) => {
+  if (err) console.error("error starting server: ", err)
+  else console.log("Server running on port 3001")
 })
-
-const mongo = require('./database')
-mongo.con
-app.use(cors())
-app.use(express.static('build'))
-
-const loginAndRegister = require('./Controllers/logInAndRegister')
-const posts = require('./Controllers/posts')
-
-app.get("/ping", (req,res) => {
-  res.json({status:true, msg:'pong'})
-})
-
-
-app.post("/refresh", loginAndRegister.refresh)
-app.post("/login", loginAndRegister.logIn)
-app.post("/register", loginAndRegister.register)
-app.get("/confirmation/:token", loginAndRegister.verifyUser)
-app.post("/resetpassword/:token", loginAndRegister.resetPassword)
-app.post("/resetpasswordlink", loginAndRegister.sendResetEmail)
-app.post("/post",loginAndRegister.verifyToken,posts.createPost)
-app.get("/post", posts.getAllPosts)
